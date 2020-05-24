4454c57a7fbc413355cb451071c2c4ddee6660b3
Srinivas Ganji <sringanj@codeaurora.org>
2020-05-24 19:02:11 +0530

    scsi: ufs: Port UFS Write Booster functionality from 4.19 to 4.14
    Down Port the UFS Write Booster functionality from 4.19 to 4.14.
    
    sysfs is not available in 4.14 for UFS and hence not ported sysfs.
    
    This feature ported from 4.19 kernel
    ------------------------------------
    2608b87 : add write booster feature support
    0197436 : configure write booster type
    d80d665 : enable write booster only if provisioned
    d3c3e45 : Remove comment to be compliant
    20ecc8a : Fix per-LU WB buffer size setting error.
    
    Change-Id: Ie3705ca53fcbf848014ecbfee37a02126241890f
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    Signed-off-by: Ziqi Chen <ziqichen@codeaurora.org>
    Signed-off-by: Srinivas Ganji <sringanj@codeaurora.org>
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -375,6 +375,7 @@ struct ufs_hba_variant_ops {
 	u32	(*get_scale_down_gear)(struct ufs_hba *);
 	int	(*set_bus_vote)(struct ufs_hba *, bool);
 	int	(*phy_initialization)(struct ufs_hba *);
+	u32	(*get_user_cap_mode)(struct ufs_hba *hba);
 #ifdef CONFIG_DEBUG_FS
 	void	(*add_debugfs)(struct ufs_hba *hba, struct dentry *root);
 	void	(*remove_debugfs)(struct ufs_hba *hba);
@@ -964,6 +965,7 @@ struct ufs_hba {
 	/* Keeps information of the UFS device connected to this host */
 	struct ufs_dev_info dev_info;
 	bool auto_bkops_enabled;
+	bool wb_buf_flush_enabled;
 
 #ifdef CONFIG_DEBUG_FS
 	struct debugfs_files debugfs_files;
@@ -1081,6 +1083,8 @@ struct ufs_hba {
 	struct keyslot_manager *ksm;
 	void *crypto_DO_NOT_USE[8];
 #endif /* CONFIG_SCSI_UFS_CRYPTO */
+
+	bool wb_enabled;
 
 	struct {
 		struct pm_qos_request req;
@@ -1538,5 +1542,12 @@ static inline void ufshcd_vops_remove_debugfs(struct ufs_hba *hba)
 {
 }
 #endif
+
+static inline unsigned int ufshcd_vops_get_user_cap_mode(struct ufs_hba *hba)
+{
+	if (hba->var && hba->var->vops->get_user_cap_mode)
+		return hba->var->vops->get_user_cap_mode(hba);
+	return 0;
+}
 
 #endif /* End of Header */
